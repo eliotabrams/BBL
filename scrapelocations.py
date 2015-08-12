92beb2494c93a313d37ca221cbb47c581de1f00f
eliotabrams <eabrams@uchicago.edu>
2015-08-11 21:08:12 -0500

    Now creates a pandas DataFrame

--- scrapelocations.py
+++ scrapelocations.py
@@ -5,7 +5,7 @@
 # Packages
 from bs4 import BeautifulSoup
 import os as os
-
+import pandas as pd
 
 # Folder locations
 os.chdir('/Users/eliotabrams/Desktop/BBL')
@@ -17,21 +17,27 @@ soup = BeautifulSoup(open("test.html"))
 
 # Parse document
 count = 0
-days=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+days = ['Sunday', 'Monday', 'Tuesday',
+        'Wednesday', 'Thursday', 'Friday', 'Saturday']
+location_data = pd.DataFrame()
+location = ''
+day = ''
 
 for thing1 in soup.find_all('td'):
     if thing1.get('style') == 'width:13%':
-        print '\n' + thing1.text
+        location = thing1.text
         count = 0
 
     else:
-        print days[count]
-        count +=1
+        day = days[count]
+        count += 1
 
     new_soup = BeautifulSoup(str(thing1), 'html.parser')
 
     for thing2 in new_soup.find_all('img'):
-        print thing2.get('title')
-        
-        
+        truck = thing2.get('title')
+        location_data = location_data.append(
+            pd.DataFrame([location, day, truck]).transpose())
 
+
+location_data = location_data.reset_index().drop('index', axis=1)
